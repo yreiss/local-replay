107a58a2b35d641e9735aabc40d2f5b61c6b9ee6
Yigal Reiss <yreiss@cisco.com>
2016-10-01 13:30:38 -0700

    move from option parser to argument parser

--- playpcap.py
+++ playpcap.py
@@ -1,5 +1,7 @@
+#! /usr/bin/env python
+
 import sys
-import optparse
+import argparse
 import socket
 import select
 import errno
@@ -9,13 +11,13 @@ import time
 
 class TunnelServer(object):
 
-    def __init__(self, taddr, tmask, tmtu, is_writer):
+    def __init__(self, taddr, tmask, tmtu, role):
         self._tun = pytun.TunTapDevice(flags=pytun.IFF_TUN|pytun.IFF_NO_PI)
         self._tun.addr = taddr
         self._tun.netmask = tmask
         self._tun.mtu = tmtu
         self._tun.up()
-        self._is_writer = is_writer
+        self._role = role
 
     def send_pcap(self):
         pkt=IP(src="10.0.1.6", dst="10.0.0.6")/UDP(dport=50000)/("hello")
@@ -27,32 +29,32 @@ class TunnelServer(object):
         mtu = self._tun.mtu
         received=''
 
-        if (self._is_writer):
+        if (self._role == 'client'):
             self.send_pcap()
             exit()
-
 
         while True:
             received = self._tun.read(mtu)
             print(":".join("{:02x}".format(ord(c)) for c in str(received)))
+
+
+def parse_args():
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("pcap", help="pcap to replay")
+    argument_parser.add_argument("taddr", help='set tunnel local address')
+    argument_parser.add_argument('--tun-netmask', default='255.255.255.0',dest='tmask', help='set tunnel netmask')
+    argument_parser.add_argument('--tun-mtu', type=int, default=1500,dest='tmtu', help='set tunnel MTU')
+    argument_parser.add_argument("--role", default='client', choices=['client', 'server'], help='send on behalf or client or server')
+
+    return argument_parser.parse_args()
+
             
 def main():
-    parser = optparse.OptionParser()
-    parser.add_option('--tun-addr', dest='taddr',
-                      help='set tunnel local address')
-    parser.add_option('--tun-netmask', default='255.255.255.0',dest='tmask',
-                      help='set tunnel netmask')
-    parser.add_option('--tun-mtu', type='int', default=1500,dest='tmtu',
-                      help='set tunnel MTU')
-    parser.add_option("-w", action="store_true", dest="writer", help='writer to tun instead of read')
-    opt, args = parser.parse_args()
-    iswriter = True if opt.writer else False
+    
+    args = parse_args()
 
-    if not opt.taddr:
-        parser.print_help()
-        return 1
     try:
-        server = TunnelServer(opt.taddr, opt.tmask, opt.tmtu, iswriter)
+        server = TunnelServer(args.taddr, args.tmask, args.tmtu, args.role)
     except (pytun.Error, socket.error), e:
             print >> sys.stderr, str(e)
             return 1
